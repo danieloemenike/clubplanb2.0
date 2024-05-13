51d0ff0179f720580e7cef275f8bf78fcf0ad387
Daniel Emenike <emenike.daniel@palmpay-inc.com>
2024-05-13 08:50:27 +0100

    style: image scaled properly

--- app/(sections)/lounge/_components/Hero.tsx
+++ app/(sections)/lounge/_components/Hero.tsx
@@ -9,8 +9,8 @@ type Props = {
 function Hero({path, title, lounge}: Props) {
   return (
       <>
-       <section className='h-[50vh] lg:h-[75vh] w-full flex flex-col pt-10 lg:flex-row mb-20 lg:pb-10'>
-              <div className='h-full  w-full flex flex-col pb:20 md:mb-1 md:justify-center justify-end items-center  lg:ml-28 lg:mr-4 mx-auto '>
+       <section className='h-[40vh] lg:h-[75vh] w-full flex flex-col pt-10 lg:flex-row mb-20 lg:pb-10'>
+              <div className='h-full  w-full flex flex-col pb:20 md:mb-1 md:justify-center justify-end items-center  mx-auto '>
                  
                   <div>
                       <h2 className='text-[2rem] md:text-[4rem] items-center justify-center flex lg:tracking-[-0.1rem] w-full lg:leading-[6rem] font-medium gap-4 font-gv tracking-tighter'>
@@ -21,9 +21,9 @@ function Hero({path, title, lounge}: Props) {
                       <h2 className="text-white/50" >{ title}</h2>
                   </div>
               </div>
-              <div className='h-[35vh] lg:h-[70vh] w-[85%] mx-auto my-auto  items-center justify-center drop-shadow-xl 
+              <div className='h-[35vh] lg:h-[70vh] w-[85%] mx-auto my-auto  items-center pt-4 justify-center drop-shadow-xl 
               '>
-                  <Image src={path} alt="model" height={400} width={400} className='rounded-3xl border-white border-2 object-cover hover:grayscale drop-shadow-xl  mx-auto my-auto h-[80%] w-[90%]  ' priority />
+                  <Image src={path} alt="model" height={400} width={400} className='rounded-3xl border-white border-2 object-cover hover:grayscale drop-shadow-xl  mx-auto my-auto h-[85%] w-[95%]  ' priority />
               </div>
       </section>
       </>
