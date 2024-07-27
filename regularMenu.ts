a4eaabbe1991696173b785276017386c8b6a778c
daniel <danieloemenike@gmail.com>
2024-07-27 22:30:03 +0100

    chivas added

--- regularMenu.ts
+++ regularMenu.ts
@@ -30,6 +30,7 @@ export const regularMenu = [
           { id: 7, name: "REMY MARTINS XO", price: "N450,000", image: "/rmxo.jpeg" },
           { id: 8, name: "REMY MARTINS VSOP", price: "N100,000", image: "/rmvs.png" },
           { id: 9, name: "REMY MARTINS 1738", price: "N150,000", image: "/rmmartins.jpeg" },
+          { id: 10, name: "CHIVAS 25 YEARS", price: "N250,000", image: "/chivas25.webp" },
         
         ]
     },
