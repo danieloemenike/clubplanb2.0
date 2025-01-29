a486e64931cb12ca4f6b1a17adeefb6e5bb3c6cc
carabanaofficial <Carabanaofficial@gmail.com>
2025-01-29 22:47:04 +0100

    Update vipMenu.ts

--- vipMenu.ts
+++ vipMenu.ts
@@ -5,6 +5,7 @@ export const vipMenu = [
         drinks: [
           { id: 0, name: "AZUL BLACK MEZCAL", price: "N700,000", image: "/blavkazul.jpeg" },
           { id: 1, name: "AZUL CLASE", price: "N600,000", image: "/azulclase.webp" },
+            { id: 2, name: "AVION RESERVA", price: "550,000", image: "/Avion-Tequila.jpg" },
           { id: 2, name: "DON JULIO", price: "N600,000", image: "/donju.png" },
           { id: 3, name: "VOLCAN XA", price: "N450,000", image: "/volxa.png" }, 
           { id: 4, name: "VOLCAN WHITE", price: "N200,000", image: "/volwhite.webp" },
