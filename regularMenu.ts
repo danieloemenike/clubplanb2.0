838127da913478136a36692db0e4cf4b4eb2c66d
carabanaofficial <Carabanaofficial@gmail.com>
2026-06-13 14:24:34 +0100

    Update regularMenu.ts

--- regularMenu.ts
+++ regularMenu.ts
@@ -143,7 +143,7 @@ export const regularMenu = [
             // { id: 5, name: "MALTA GUINNESS", price: "N800", image: "/mltg.jpeg" },
             // { id: 6, name: "AMSTEL MALTA ", price: "N800", image: "/amsmalt.png" },
             // { id: 7, name: "HANGOVER TEA ", price: "N1,000", image: "/hgov.jpeg" },
-            { id: 8, name: "REDBULL", price: "N5,000", image: "/3ddbb24e4675ae706b2f571ed55ca987.jpg" },//CHECK
+            { id: 8, name: "REDBULL ENERGY DRINK", price: "N5,000", image: "/3ddbb24e4675ae706b2f571ed55ca987.jpg" },//CHECK
             { id: 9, name: "BIG CHIVITA", price: "N8,000", image: "/chibig.jpeg" },
             // { id: 10, name: "SMALL CHIVITA", price: "N700", image: "/chismall.jpeg" },//CHECK
             { id: 11, name: "CRANBERRY", price: "N15,000", image: "/stute.png" },
