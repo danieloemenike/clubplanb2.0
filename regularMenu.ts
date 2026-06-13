05002460c76051862d5fafce2899bbc1924b56dc
carabanaofficial <Carabanaofficial@gmail.com>
2026-06-13 18:43:31 +0100

    Update regularMenu.ts

--- regularMenu.ts
+++ regularMenu.ts
@@ -143,7 +143,7 @@ export const regularMenu = [
             // { id: 5, name: "MALTA GUINNESS", price: "N800", image: "/mltg.jpeg" },
             // { id: 6, name: "AMSTEL MALTA ", price: "N800", image: "/amsmalt.png" },
             // { id: 7, name: "HANGOVER TEA ", price: "N1,000", image: "/hgov.jpeg" },
-            { id: 8, name: "RED BULL", price: "N5,000", image: "/1234.png" },//CHECK
+            { id: 8, name: "RED BULL", price: "N5,000", image: "Screenshot_20260613-183258.jpg" },//CHECK
             { id: 9, name: "REDBULL SUGAR FREE", price: "N5,000", image: "/3fc61c3a-e79d-4f7e-8445-6efa3d1323be_728080430.jpeg"},
             { id: 9, name: "REDBULL WATERMELON", price: "N5,000", image: "/Red-Bull-Red-12oz-Cans_1.jpeg" },
             { id: 9, name: "BIG CHIVITA", price: "N8,000", image: "/chibig.jpeg" },
