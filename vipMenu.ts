6f163694ab9634734be9a7710057074cc0a80748
carabanaofficial <Carabanaofficial@gmail.com>
2026-06-13 14:25:42 +0100

    Update vipMenu.ts

--- vipMenu.ts
+++ vipMenu.ts
@@ -136,7 +136,7 @@ export const vipMenu = [
             // { id: 5, name: "MALTA GUINNESS", price: "N800", image: "/mltg.jpeg" },
             // { id: 6, name: "AMSTEL MALTA ", price: "N800", image: "/amsmalt.png" },
            // { id: 7, name: "HANGOVER TEA ", price: "N2,000", image: "/hgov.jpeg" },
-            { id: 8, name: "REDBULL", price: "N5,000", image: "/IMG_20260508_003119.jpg" },//CHECK
+            { id: 8, name: "REDBULL", price: "N5,000", image: "/3ddbb24e4675ae706b2f571ed55ca987.jpg" },//CHECK
              { id: 9, name: "BIG CHIVITA", price: "N8,000", image: "/chibig.jpeg" },//CHECK
             // { id: 10, name: "SMALL CHIVITA", price: "N700", image: "/chismall.jpeg" },//CHECK
             { id: 11, name: "CRANBERRY", price: "N15,000", image: "/stute.png" },
